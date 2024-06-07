2c1ac3f9e980e89a4f264905270e20dfc2fd7b18
Kevin <esfk070@gmail.com>
2024-06-07 12:27:01 -0600

    complete objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,16 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    int year = 1988;
+    String month = "September";
+    boolean weekend = false;
+
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(weekend);
+    
+
+
+
+  }
+}
